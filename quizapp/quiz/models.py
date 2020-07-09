0571a612c2a521b12ce87b7090c792e841edfe11
azanbinzahid <azan.zahid@arbisoft.com>
2020-07-09 20:13:01 +0500

    create models

--- quizapp/quiz/models.py
+++ quizapp/quiz/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Quiz(models.Model):
+    quiz_desc = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    correct_ans = models.BooleanField(default=False)
