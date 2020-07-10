25ba3bdee14c406f21bb1070629a2be000695e63
azanbinzahid <azan.zahid@arbisoft.com>
2020-07-10 11:40:01 +0500

    add simple view

--- quizapp/quiz/views.py
+++ quizapp/quiz/views.py
@@ -2,7 +2,23 @@
 
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Quiz, Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_quiz_list = Quiz.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_quiz_list': latest_quiz_list,
+    }
+    return render(request, 'quiz/index.html', context)
+
+def detail(request, quiz_id):
+    return HttpResponse("You're looking at question %s." % quiz_id)
+
+def results(request, quiz_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % quiz_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
