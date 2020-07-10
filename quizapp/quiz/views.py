62dd08fcfee06d13cc7b5051dc093b017bb0d85b
azanbinzahid <azan.zahid@arbisoft.com>
2020-07-10 12:39:35 +0500

    add views for attpeting questions

--- quizapp/quiz/views.py
+++ quizapp/quiz/views.py
@@ -1,9 +1,10 @@
 # from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from .models import Quiz, Question
+from django.urls import reverse
+from .models import Quiz, Question, Choice
 
 
 def index(request):
@@ -14,11 +15,33 @@ def index(request):
     return render(request, 'quiz/index.html', context)
 
 def detail(request, quiz_id):
-    return HttpResponse("You're looking at question %s." % quiz_id)
+    quiz = Quiz.objects.get(pk=quiz_id)
+    context = {
+        "quiz" : quiz,
+    }
+    return render(request, 'quiz/detail.html', context)
 
-def results(request, quiz_id):
+
+def results(request, question_id):
     response = "You're looking at the results of question %s."
-    return HttpResponse(response % quiz_id)
+    return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = Question.objects.get(pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'quiz/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        score = 0
+        if selected_choice.correct_ans == True:
+            score = 10
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('quiz:results', args=(question.id,)))
