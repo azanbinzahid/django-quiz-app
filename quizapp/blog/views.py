607633d8de417b30b85b8c5e10f03dfd89bb0f01
azanbinzahid <azan.zahid@arbisoft.com>
2020-07-13 11:49:32 +0500

    basic API setup

--- quizapp/blog/views.py
+++ quizapp/blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from .models import Blog
+from .serializers import BlogSerializer
+from rest_framework import generics
 
-# Create your views here.
+class BlogListCreate(generics.ListCreateAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
